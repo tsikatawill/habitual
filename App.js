fad1f1ba0753da1a8804cc2258c20b859245445f
William M. Tsikata <tsikatawill@gmail.com>
2023-08-28 00:26:15 +0000

    populate homescreen

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text } from "react-native";
+import CustomBottomSheet from "./components/CustomBottomSheet";
+import { useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import Header from "./components/Header";
+import FontsProvider from "./components/FontsProvider";
+import HabitsGrid from "./components/HabitsGrid";
+import { useModal } from "./store/useStore";
+import useBottomSheet from "./hooks/useBottomSheet";
 
 export default function App() {
+  const updateContent = useModal((state) => state.updateContent);
+  const isOpen = useModal((state) => state.isOpen);
+  const handleDismiss = useModal((state) => state.handleDismiss);
+  const updateRef = useModal((state) => state.updateRef);
+
+  const { bottomSheetRef } = useBottomSheet();
+
+  useEffect(() => {
+    const handleModalInitials = () => {
+      updateRef(bottomSheetRef);
+      updateContent(<Text>Initial Modal Content</Text>);
+    };
+
+    handleModalInitials();
+  }, [bottomSheetRef]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <FontsProvider>
+          <ScrollView>
+            <StatusBar style="auto" />
+            <Header />
+
+            <HabitsGrid />
+          </ScrollView>
+        </FontsProvider>
+      </SafeAreaView>
+
+      <CustomBottomSheet
+        ref={bottomSheetRef}
+        handleDismiss={handleDismiss}
+        showModal={isOpen}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
